5aa221d8726740d6a3497d1b6a03b28469ef36d8
panin-ananwa <pananwatanyoo@tudelft.nl>
2024-12-13 10:10:28 +0000

    add user choice for volume_predictor_svr branches

--- grinder_model/volume_predictor_svr.py
+++ grinder_model/volume_predictor_svr.py
@@ -130,7 +130,18 @@ def evaluate_model_feed_rate_tag(model, X_test, y_test, OG_grind_data):
     plt.tight_layout()
     plt.show()
 
+def get_user_choice():
+    print("Choose an option:")
+    print("1. Use manual data input")
+    print("2. Load CSV data for evaluation")
+    choice = input("Enter 1 or 2: ")
+    return choice
+
+
 def main():
+
+    user_choice =  get_user_choice()
+
     #get grind model
     use_fixed_model_path = True# Set this to True or False based on your need
     
@@ -146,62 +157,62 @@ def main():
         grind_model = load_model(use_fixed_path=False)
         scaler = load_scaler(use_fixed_path=False)
 
+    if user_choice == "1":
+        #read current belt's 'initial wear', 'removed_volume', 'RPM' and predict 'Force' and 'grind_time'
+        rpm_range = np.arange(9000, 9510, 500)  # from 8500 to 10000 in steps of 500
+        force_range = np.arange(5, 5.1, 1)  # from 3 to 9 in steps of 1
+        time_range = np.arange(12.5, 12.6, 0.5)
+        grind_area = 50
+        initial_wear = 30000000
+        feed_rate = 10
+        num_pass = 8
+        pass_length = 100
+        belt_width = 25
+
+        for avg_rpm in rpm_range:
+           for avg_force in force_range:
+               for grind_time in time_range:
+                    # Create a DataFrame to store the input data
+                    input_data_dict = {
+                        'grind_time': [grind_time],
+                        'avg_rpm': [avg_rpm],
+                        'avg_force': [avg_force],
+                        'grind_area': [grind_area],
+                        'initial_wear': [initial_wear]
+                    }
+                    input_df = pd.DataFrame(input_data_dict)
+                    input_scaled = scaler.transform(input_df)
+                    input_scaled = pd.DataFrame(input_scaled, columns=input_df.columns)
+                    # Predict volume
+                    predicted_volume = grind_model.predict(input_scaled)
+                    predicted_total_vol = predicted_volume[0]*pass_length/belt_width
+                    print(f"RPM: {avg_rpm}, Force: {avg_force}N, grind_area: {grind_area}mm^2, Grind Time: {grind_time} sec --> Predicted Removed Volume: {predicted_volume[0]}")
+                    #print(f"Grind Time: {grind_time} sec --> Feed_rate: {feed_rate} mm/s, Num_pass: {num_pass}, predicted_total_volume: {predicted_total_vol}")
+    if user_choice == "2":    
+        #load test data and evaluate model
+        #read grind data
+        data_manager = DataManager()
+        grind_data = data_manager.load_data()
     
-    #read current belt's 'initial wear', 'removed_volume', 'RPM' and predict 'Force' and 'grind_time'
-    rpm_range = np.arange(9000, 9510, 500)  # from 8500 to 10000 in steps of 500
-    force_range = np.arange(5, 5.1, 1)  # from 3 to 9 in steps of 1
-    time_range = np.arange(12.5, 12.6, 0.5)
-    grind_area = 50
-    initial_wear = 30000000
-    feed_rate = 10
-    num_pass = 8
-    pass_length = 100
-    belt_width = 25
+        #filter out points that has high mad_rpm, material removal of less than 5, duplicates, failure msg detected
+        grind_data = data_manager.filter_grind_data()
+        grind_data['index'] = grind_data.index
+        grind_data['grind_time'] = grind_data['contact_time'] / 4
+        OG_grind_data = grind_data                      #for tagging different parameter value settings in colors
     
-    for avg_rpm in rpm_range:
-       for avg_force in force_range:
-           for grind_time in time_range:
-                # Create a DataFrame to store the input data
-                input_data_dict = {
-                    'grind_time': [grind_time],
-                    'avg_rpm': [avg_rpm],
-                    'avg_force': [avg_force],
-                    'grind_area': [grind_area],
-                    'initial_wear': [initial_wear]
-                }
-                input_df = pd.DataFrame(input_data_dict)
-                input_scaled = scaler.transform(input_df)
-                input_scaled = pd.DataFrame(input_scaled, columns=input_df.columns)
-                # Predict volume
-                predicted_volume = grind_model.predict(input_scaled)
-                predicted_total_vol = predicted_volume[0]*pass_length/belt_width
-                print(f"RPM: {avg_rpm}, Force: {avg_force}N, grind_area: {grind_area}mm^2, Grind Time: {grind_time} sec --> Predicted Removed Volume: {predicted_volume[0]}")
-                #print(f"Grind Time: {grind_time} sec --> Feed_rate: {feed_rate} mm/s, Num_pass: {num_pass}, predicted_total_volume: {predicted_total_vol}")
+        print(grind_data)
     
-    #load test data and evaluate model
-    #read grind data
-    data_manager = DataManager()
-    grind_data = data_manager.load_data()
-
-    #filter out points that has high mad_rpm, material removal of less than 5, duplicates, failure msg detected
-    grind_data = data_manager.filter_grind_data()
-    grind_data['index'] = grind_data.index
-    grind_data['grind_time'] = grind_data['contact_time'] / 4
-    OG_grind_data = grind_data                      #for tagging different parameter value settings in colors
-
-    print(grind_data)
-
-    #drop unrelated columns
-    related_columns = ['grind_time', 'avg_rpm', 'avg_force', 'grind_area', 'initial_wear', 'removed_material', 'index', 'feed_rate_setpoint']
-    grind_data = grind_data[related_columns]
-
-    #desired output
-    target_columns = ['removed_material', 'index', 'feed_rate_setpoint']
-
-    # Preprocess the data (train the model using the CSV data, for example)
-    X_test_scaled, y_test = preprocess_test_data(grind_data, target_columns, scaler)
-
-    evaluate_model_feed_rate_tag(grind_model, X_test_scaled, y_test, OG_grind_data)
+        #drop unrelated columns
+        related_columns = ['grind_time', 'avg_rpm', 'avg_force', 'grind_area', 'initial_wear', 'removed_material', 'index', 'feed_rate_setpoint']
+        grind_data = grind_data[related_columns]
+    
+        #desired output
+        target_columns = ['removed_material', 'index', 'feed_rate_setpoint']
+    
+        # Preprocess the data (train the model using the CSV data, for example)
+        X_test_scaled, y_test = preprocess_test_data(grind_data, target_columns, scaler)
+    
+        evaluate_model_feed_rate_tag(grind_model, X_test_scaled, y_test, OG_grind_data)
     
     
 if __name__ == "__main__":
