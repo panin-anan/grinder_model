fa9b3d50115f6d9a59285a6b382917f75e45477d
panin-ananwa <pananwatanyoo@tudelft.nl>
2024-12-13 10:08:22 +0000

    add user choice for volume_predictor_svr

--- grinder_model/volume_predictor_svr.py
+++ grinder_model/volume_predictor_svr.py
@@ -68,9 +68,19 @@ def preprocess_test_data(data, target_column, scaler):
 
     return X_test_scaled, y_test
 
+def get_user_choice():
+    print("Choose an option:")
+    print("1. Use manual data input")
+    print("2. Load CSV data for evaluation")
+    choice = input("Enter 1 or 2: ")
+    return choice
+
 def main():
+
+    user_choice = get_user_choice()
+
     #get grind model
-    use_fixed_model_path = True# Set this to True or False based on your need
+    use_fixed_model_path = True     # Set this to True or False based on your need of repetitive test on the same model path
     
     if use_fixed_model_path:
         # Specify the fixed model and scaler paths
@@ -84,57 +94,56 @@ def main():
         grind_model = load_model(use_fixed_path=False)
         scaler = load_scaler(use_fixed_path=False)
 
-    
-    #read current belt's 'initial wear', 'removed_volume', 'RPM' and predict 'Force' and 'grind_time'
-    rpm_range = np.arange(10500, 11100, 100)  # from 8500 to 10000 in steps of 500
-    force_range = np.arange(4, 5.1, 1)  # from 3 to 9 in steps of 1
-    time_range = np.arange(10.0, 10.5, 0.5)
-    grind_area = 15
-    initial_wear = 10000000
+    if user_choice == "1":
+        #Manually input a range of input
+        rpm_range = np.arange(9500, 9510, 100)  # from 8500 to 10000 in steps of 500
+        force_range = np.arange(5, 5.1, 1)  # from 3 to 9 in steps of 1
+        time_range = np.arange(12.5, 12.6, 0.5)
+        grind_area = 50
+        initial_wear = 30000000
 
-    for avg_rpm in rpm_range:
-       for avg_force in force_range:
-           for grind_time in time_range:
-                # Create a DataFrame to store the input data
-                input_data_dict = {
-                    'grind_time': [grind_time],
-                    'avg_rpm': [avg_rpm],
-                    'avg_force': [avg_force],
-                    'grind_area': [grind_area],
-                    'initial_wear': [initial_wear]
-                }
-                input_df = pd.DataFrame(input_data_dict)
-                input_scaled = scaler.transform(input_df)
-                input_scaled = pd.DataFrame(input_scaled, columns=input_df.columns)
-                # Predict volume
-                predicted_volume = grind_model.predict(input_scaled)
-                print(f"RPM: {avg_rpm}, Force: {avg_force}N, grind_area: {grind_area}mm^2, Grind Time: {grind_time} sec --> Predicted Removed Volume: {predicted_volume[0]}")
-    
-    '''
-    #load test data and evaluate model
-    #read grind data
-    data_manager = DataManager()
-    grind_data = data_manager.load_data()
+        for avg_rpm in rpm_range:
+           for avg_force in force_range:
+               for grind_time in time_range:
+                    # Create a DataFrame to store the input data
+                    input_data_dict = {
+                        'grind_time': [grind_time],
+                        'avg_rpm': [avg_rpm],
+                        'avg_force': [avg_force],
+                        'grind_area': [grind_area],
+                        'initial_wear': [initial_wear]
+                    }
+                    input_df = pd.DataFrame(input_data_dict)
+                    input_scaled = scaler.transform(input_df)
+                    input_scaled = pd.DataFrame(input_scaled, columns=input_df.columns)
+                    # Predict volume
+                    predicted_volume = grind_model.predict(input_scaled)
+                    print(f"RPM: {avg_rpm}, Force: {avg_force}N, grind_area: {grind_area}mm^2, Grind Time: {grind_time} sec --> Predicted Removed Volume: {predicted_volume[0]}")
 
-    #filter out points that has high mad_rpm, material removal of less than 5, duplicates, failure msg detected
-    grind_data = data_manager.filter_grind_data()
-    grind_data['index'] = grind_data.index
-    OG_grind_data = grind_data
+    if user_choice == "2":
+        #load test data and evaluate model
+        #read grind data
+        data_manager = DataManager()
+        grind_data = data_manager.load_data()
 
-    print(grind_data)
+        #filter out points that has high mad_rpm, material removal of less than 5, duplicates, failure msg detected
+        grind_data = data_manager.filter_grind_data()
+        grind_data['index'] = grind_data.index
+        OG_grind_data = grind_data
 
-    #drop unrelated columns
-    related_columns = ['grind_time', 'avg_rpm', 'avg_force', 'grind_area', 'initial_wear', 'removed_material', 'index']
-    grind_data = grind_data[related_columns]
+        print(grind_data)
 
-    #desired output
-    target_columns = ['removed_material', 'index']
+        #drop unrelated columns
+        related_columns = ['grind_time', 'avg_rpm', 'avg_force', 'grind_area', 'initial_wear', 'removed_material', 'index']
+        grind_data = grind_data[related_columns]
 
-    # Preprocess the data (train the model using the CSV data, for example)
-    X_test_scaled, y_test = preprocess_test_data(grind_data, target_columns, scaler)
+        #desired output
+        target_columns = ['removed_material', 'index']
 
-    evaluate_model(grind_model, X_test_scaled, y_test, OG_grind_data)
-    '''
+        # Preprocess the data (train the model using the CSV data, for example)
+        X_test_scaled, y_test = preprocess_test_data(grind_data, target_columns, scaler)
+
+        evaluate_model(grind_model, X_test_scaled, y_test, OG_grind_data)
 
 if __name__ == "__main__":
     main()
